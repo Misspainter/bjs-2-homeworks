9ff53659f4623f6c715c42252d6e2abec52f5efb
Misspainter <117083864+Misspainter@users.noreply.github.com>
2023-07-22 23:04:02 +0300

    Третье задание готово с ошибками

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+	compare = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+	return compare;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let filtetUser = users.filter(user => user.gender === gender);
+	let userAge = filtetUser.map(user => user.age);
+
+	if (!users.length) {
+		return 0;
+	} else if (gender == undefined) {
+		return 0;
+	} else {
+		let result = userAge.reduce((sumAge, ageValue) => {
+			return (sumAge + ageValue) / userAge.length;
+		}, 0)
+	}
 }
